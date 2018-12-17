f7738158ba291c91909de364001f610769b84502
Adam Lukens <adam@testdouble.com>
2018-12-17 01:24:10 -0500

    Show progress towards goal

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,8 +2,16 @@ class Account < ApplicationRecord
   belongs_to :snowball
   has_many :transactions
 
+  def initial_balance
+    transactions.order(:date).first.amount_cents
+  end
+
   def current_balance
     transactions.map(&:amount_cents).sum
+  end
+
+  def current_difference
+    current_balance - initial_balance
   end
 
   def to_s
