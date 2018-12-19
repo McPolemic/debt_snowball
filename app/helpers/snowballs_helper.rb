2f7f13ac85d478d80a855a0158ae61f509685912
Adam Lukens <adam@testdouble.com>
2018-12-19 01:59:12 -0500

    Color money differences in Snowball#show

--- app/helpers/snowballs_helper.rb
+++ app/helpers/snowballs_helper.rb
@@ -1,2 +1,11 @@
 module SnowballsHelper
+  def money_color_class(amount)
+    if amount >  0
+      'money-positive'
+    elsif amount < 0
+      'money-negative'
+    else
+      ''
+    end
+  end
 end
